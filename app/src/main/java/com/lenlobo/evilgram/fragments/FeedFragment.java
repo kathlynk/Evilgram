b7834bc1059225381be7fa55a448e287371e334b
Len LoBo <llobiondo2@gmail.com>
2020-10-22 14:13:20 -0700

    feed ui buttons added.  Feed ui updated

--- app/src/main/java/com/lenlobo/evilgram/fragments/FeedFragment.java
+++ app/src/main/java/com/lenlobo/evilgram/fragments/FeedFragment.java
@@ -3,6 +3,7 @@ package com.lenlobo.evilgram.fragments;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
